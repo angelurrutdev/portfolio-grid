1c69d6bece24ace00c8bece3cf6c41b082cbaa94
angelurrutdev <angeljurrut@gmail.com>
2024-07-10 10:01:36 -0400

    Solutions

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -22,22 +22,18 @@ export const DevTools: IDevToolsElement[] = [
 		name: 'JavaScript',
 		icon: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg',
 	},
-	/* {
+
+	{
 		id: uuid(),
-		name: 'TypeScript',
-		icon: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
-	}, */
-	/* 	{
-			id: uuid(),
-			name: 'React',
-			icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
-		}, */
-	/* {
+		name: 'React',
+		icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+	},
+	{
 		id: uuid(),
-		name: 'Next.js',
-		icon: 'https://www.svgrepo.com/show/354113/nextjs-icon.svg',
-		className: 'dark:invert opacity-70',
-	}, */
+		name: 'Astro 4',
+		icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Astro.svg/350px-Astro.svg.png',
+		className: 'dark:invert opacity-70 h-[60px] w-[50px]',
+	},
 	{
 		id: uuid(),
 		name: 'Vite',
@@ -48,6 +44,10 @@ export const DevTools: IDevToolsElement[] = [
 		name: 'Tailwind CSS',
 		icon: 'https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg',
 	},
-
-
 ]
+
+/* {
+		id: uuid(),
+		name: 'TypeScript',
+		icon: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+	}, */
